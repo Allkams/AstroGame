a0137be1749d57c2c2fb23ba9afd7b3901ae6247
Allkams <Allkams02@gmail.com>
2023-04-12 19:06:18 +0200

    First update

---
+++ TheGame/Assets/Scripts/PlayerScripts/MovementScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool playerOnGround;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.82f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.AddComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerOnGround = controller.isGrounded;
+
+        if(playerOnGround && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * playerSpeed);
+        
+        if(move != Vector3.zero)
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if(Input.GetButtonDown("Jump") && playerOnGround)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+    }
+}
